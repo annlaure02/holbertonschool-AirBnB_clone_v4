969562e6ddfd9823bebfb30cbb765d8aac2d40d1
annlaure02 <alaure.g02@gmail.com>
2023-04-25 16:36:41 +0200

    modify css file and html

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  let new_list = [];
+  $('input[type="checkbox"]').change(function() {
+    if($(this).prop('checked')) {
+      new_list.push($(this).attr('data-id'));
+    } else {
+      new_list = new_list.filter(id => id !== $(this).attr('data-id'));
+    }
+
+    let amenitiesText = '';
+    for (let id of new_list) {
+      const name = $('input[data-id="'+id+'"]').attr('data-name');
+      amenitiesText += name + ', ';
+    }
+    amenitiesText = amenitiesText.slice(0, -2);
+    $('.amenities h4').text(amenitiesText);
+  });
+});
